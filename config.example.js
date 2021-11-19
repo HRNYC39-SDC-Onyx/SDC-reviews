cbf4811f3e034142be4f548e7e032a3a071b4e3c
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-18 20:51:45 -0500

    example config file

---
+++ config.example.js
@@ -0,0 +1,10 @@
+// If you do not have a password saved, set value to empty string
+// Change PORT NUMBER from string to number
+
+module.exports = {
+  user: 'YOUR USER',
+  host: 'YOUR LOCAL HOST',
+  database: 'YOUR DATABASE',
+  password: 'YOUR PASSWORD',
+  port: 'PORT NUMBER',
+}
