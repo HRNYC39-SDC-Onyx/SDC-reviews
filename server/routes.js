fe74edd700298287226b4fe9ea32debeb4ded221
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-19 23:42:33 -0500

    api routes

---
+++ server/routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+const controller = require('./controllers')
+
+router.get('/', controller.reviews.get)
+// router.post('/', controller.reviews.post)
+
+// router.get('/meta', controller.meta.get)
+
+router.put('/:review_id/helpful', controller.helpful.put)
+
+router.put('/:review_id/report', controller.report.put)
+
+module.exports = router
