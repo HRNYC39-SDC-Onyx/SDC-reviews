7bb4e0970d1c0fd451a3ae3aa062007fd54392a5
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-19 23:41:09 -0500

    added router file

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const router = require('./routes.js')
 
 const app = express()
 const port = 3000
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+app.use('/reviews', router)
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`)
