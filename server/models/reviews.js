68ca1fedfe575042d4694829467a749b7e8ebfe1
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-20 15:54:13 -0500

    added Photos model and linked foreign keys

--- server/models/reviews.js
+++ server/models/reviews.js
@@ -1,4 +1,5 @@
 const { DataTypes } = require('sequelize')
+const { reviews } = require('.')
 const db = require('../../db')
 
 const Review = db.define('reviews', {
@@ -18,6 +19,17 @@ const Review = db.define('reviews', {
   timestamps: false
 })
 
+const Photos = db.define('photos', {
+  id: { type: DataTypes.INTEGER, primaryKey: true },
+  review_id: { type: DataTypes.INTEGER },
+  url: { type: DataTypes.TEXT }
+}, {
+  timestamps: false
+})
+
+Review.hasMany(Photos, { foreignKey: 'review_id' })
+Photos.belongsTo(Review, { foreignKey: 'id' })
+
 module.exports = {
   Review,
   getReviews: (product_id, cb) => {
@@ -26,6 +38,12 @@ module.exports = {
         product_id: product_id
       },
       raw: true,
+      include: [
+        {
+          model: Photos,
+          raw: true
+        }
+      ]
       // limit: 10
     })
     .then(r => !r.length ? cb(err) : cb(null, r))
