093260bddeb2305f701d40d452a9a2d19b84c348
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-19 23:39:13 -0500

    updated to include data for sequelize connect

--- config.example.js
+++ config.example.js
@@ -1,10 +1,7 @@
-// If you do not have a password saved, set value to empty string
 // Change PORT NUMBER from string to number
 
 module.exports = {
-  user: 'YOUR USER',
   host: 'YOUR LOCAL HOST',
   database: 'YOUR DATABASE',
-  password: 'YOUR PASSWORD',
-  port: 'PORT NUMBER',
+  port: 'PORT NUMBER'
 }
