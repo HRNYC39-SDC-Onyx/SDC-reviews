438dc532117ce1da7642ef122e3e6a4f5b3b92c2
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-20 15:55:18 -0500

    cleaned up code

--- server/models/helpful.js
+++ server/models/helpful.js
@@ -2,7 +2,7 @@ const { Review } = require('./reviews.js')
 
 module.exports = {
   markHelpful: (review_id, cb ) => {
-    Review.increment('helpfulness', { by: 1, where: { id: review_id }, returning: false})
+    Review.increment('helpfulness', { by: 1, where: { id: review_id }, returning: false })
     .then(r => cb(null))
     .catch(err => cb(err))
   }
--- server/models/report.js
+++ server/models/report.js
@@ -2,7 +2,6 @@ const { Review } = require('./reviews.js')
 
 module.exports = {
   reportReview: (review_id, cb) => {
-    console.log(review_id)
     Review.update({
       reported: true
     }, { where: { id: review_id }})
