91f39f17a13d7bf0ff61293d6a857b56a6e14b3a
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-24 18:14:55 -0500

    refactored code to use async/await

--- server/models/helpful.js
+++ server/models/helpful.js
@@ -1,9 +1,11 @@
-const { Review } = require('./reviews.js')
+const { Review } = require("./reviews.js");
 
 module.exports = {
-  markHelpful: (review_id, cb ) => {
-    Review.increment('helpfulness', { by: 1, where: { id: review_id }, returning: false })
-    .then(r => cb(null))
-    .catch(err => cb(err))
-  }
-}
+  markHelpful: (review_id) => {
+    return Review.increment("helpfulness", {
+      by: 1,
+      where: { id: review_id },
+      returning: false,
+    });
+  },
+};
