d0f2f6033f49023850434d9da25f17bbb4e6f121
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-24 18:15:49 -0500

    refactored code to use async/await

--- server/controllers/report.js
+++ server/controllers/report.js
@@ -1,11 +1,17 @@
-const models = require('../models')
+const models = require("../models");
 
 module.exports = {
-  put: (req, res) => {
-    const { review_id } = req.params
-    !review_id && res.status(404).send('Error: invalid review id provided')
-    models.report.reportReview(review_id, err => {
-      err ? res.status(500).send('An error occurred. If this error persists, contact your instruction team') : res.status(204).send()
-    })
-  }
-}
+  put: async (req, res) => {
+    try {
+      const { review_id } = req.params;
+      await models.report.reportReview(review_id);
+      res.status(204).send();
+    } catch (err) {
+      res
+        .status(500)
+        .send(
+          "An error occurred. If this error persists, contact your instruction team"
+        );
+    }
+  },
+};
--- server/models/report.js
+++ server/models/report.js
@@ -1,11 +1,12 @@
-const { Review } = require('./reviews.js')
+const { Review } = require("./reviews.js");
 
 module.exports = {
-  reportReview: (review_id, cb) => {
-    Review.update({
-      reported: true
-    }, { where: { id: review_id }})
-    .then(r => cb(null))
-    .catch(err => cb(err))
-  }
-}
+  reportReview: (review_id) => {
+    return Review.update(
+      {
+        reported: true,
+      },
+      { where: { id: review_id } }
+    );
+  },
+};
