0e5f7599b191d84d610a573d051b3cb311ad94e4
jaimienakayama <jaimie.nakayama@icloud.com>
2021-11-26 16:55:59 -0500

    changed default page to be 1 in reviews get

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -3,7 +3,7 @@ const models = require("../models");
 module.exports = {
   get: async (req, res) => {
     try {
-      const { product_id, page = 0, count = 5 } = req.query;
+      const { product_id, page = 1, count = 5 } = req.query;
       !product_id && res.status(422).send("Error: invalid product_id provided");
       const reviews = await models.reviews.getReviews(product_id, page, count);
       res.status(200).send(reviews);
